8e9f3d9c89f7fec39c026751213ee3f5b70b66d8
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-04-26 16:11:20 +0900

    02

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public Transform transform;
+    public Vector3 respown =new Vector3(-23.7f,2.5f,-28.4f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -5)
+        {
+            Debug.Log("GameOver!!!");
+            transform.position = new Vector3(-23.7f, 2.5f, -28.4f);
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+        }
+    }
+}
